21b7389e13fbfa1c2c7ff34c7c193714bcc27253
ANGEL-141 <umranawaka@gmail.com>
2020-02-23 23:48:49 +0530

    Remove all
    Remove all

--- src/Bank.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-import java.util.*;
-
-interface Bank extends Remote{
-    public List<Customer> getCustomers()throws RemoteException;
-}
--- src/MyServer.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-public class MyServer{
-    public static void main(String args[])throws Exception{
-        Remote r=new BankImpl();
-        Naming.rebind("rmi://localhost:6666/RMI_GUI",r);
-    }}
